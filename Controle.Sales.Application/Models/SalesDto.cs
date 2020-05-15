2094da0fb927db6ff39d2760cdbaea6a39d9e4ac
Kelvin Ramirez <kramirez27@outlook.es>
2020-05-15 11:59:27 +0200

    Refactorizando codigo

--- Controle.Sales.Application/Models/SalesDto.cs
+++ Controle.Sales.Application/Models/SalesDto.cs
@@ -10,30 +10,30 @@ namespace Controle.Sales.Application.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        public string Region { get; set; }
+        public string Region { get; set; } // Region 
 
-        public string Country { get; set; }
+        public string Country { get; set; } // Pais
 
-        public string Item_Type { get; set; }
+        public string Item_Type { get; set; } // Tipo de item
 
-        public string Sales_Channel { get; set; }
+        public string Sales_Channel { get; set; } // Canal de Venta
 
-        public string Order_Priority { get; set; }
+        public string Order_Priority { get; set; } // Prioridad de orden
 
-        public string Order_Date { get; set; }
+        public string Order_Date { get; set; } // Fecha de Orden
 
-        public string Ship_Date { get; set; }
+        public string Ship_Date { get; set; } // Fecha de envío 
 
-        public Double Units_Sold { get; set; }
+        public Double Units_Sold { get; set; } // Unidades Vendidas
 
-        public Double Unit_Price { get; set; }
+        public Double Unit_Price { get; set; } // Precio Unitario
 
-        public Double Unit_Cost { get; set; }
+        public Double Unit_Cost { get; set; } // Costo Unitario
 
-        public Double Total_Revenue { get; set; }
+        public Double Total_Revenue { get; set; } // Ingresos totales
 
-        public Double Total_Cost { get; set; }
+        public Double Total_Cost { get; set; } // Coste Total
 
-        public Double Total_Profit { get; set; }
+        public Double Total_Profit { get; set; } // Benefocio Total
     }
 }
